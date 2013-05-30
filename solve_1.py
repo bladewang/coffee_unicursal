534cd24651d88ca055889b4d16f84e21e41a2d95
bladewang <k.y.wang.1911@gmail.com>
2013-05-31 02:41:55 +0800

    fix test case of solve_1.py

--- solve_1.py
+++ solve_1.py
@@ -16,9 +16,9 @@ def cmp_lsum(a, b):
 
 if __name__ == '__main__':
 
-    from solve_helper import gen_random_points
+    from solve_helper import gen_random_points, path_permutations
 
-    pl = path_permutations(100, 100, 4)
+    pl = path_permutations(gen_random_points(100, 100, 4))
 
     spl = sorted(pl, cmp=cmp_lsum)
 
