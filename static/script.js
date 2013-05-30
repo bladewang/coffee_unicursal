63da4bce2a8d59b1fc16c739584982e7009fce6d
bladewang <k.y.wang.1911@gmail.com>
2013-05-30 09:32:06 +0800

    标记端点；画更直的线。

--- static/script.js
+++ static/script.js
@@ -49,15 +49,15 @@ coffee_draw = (pl, delay_factor=0.3) ->
           point_idx += 1
           [tp_x, tp_y] = point_list[point_idx]
           delay = delay_to_p(x, y, tp_x, tp_y) 
+          @stroke(200, 0, 0)
+          @strokeWeight(1)
+          @ellipse x, y, 10, 10
 
         [od_x, od_y] = [x, y]
         x += (tp_x - x) / delay
         y += (tp_y - y) / delay
 
         delay -= 1
-
-        x = Math.round x
-        y = Math.round y
         
         @stroke(200)
         @strokeWeight(5);
