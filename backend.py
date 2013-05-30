e65beb86d8ab287baef3782445a74cd11adf0e3f
bladewang <k.y.wang.1911@gmail.com>
2013-05-30 09:01:33 +0800

    python app worked

--- backend.py
+++ backend.py
@@ -1,6 +1,6 @@
 # coding: utf-8
 
-from flask import Flask
+from flask import Flask, render_template
 from json import dumps as json_dumps
 from solve_helper import gen_random_points
 
@@ -8,7 +8,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return "hello_world"
+    return render_template('index.html')
+
 
 @app.route('/rand_points/<int:width>/<int:height>/<int:count>')
 def rand_points(width=100, height=100, count=8):
