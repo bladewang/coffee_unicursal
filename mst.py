44904653456f7b8b6e36052096c38cf128a1a9df
bladewang <k.y.wang.1911@gmail.com>
2013-06-02 03:05:02 +0800

    mst worked.

--- mst.py
+++ mst.py
@@ -23,26 +23,31 @@ def fullmap_of_pointslist(point_list, edge_length=distance_2d):
 
 def mst(edge_list):
     '''
+    calculate Min Spanning Tree by kruskal algorithm.
     edge_list: [
         (((0,0), (1,1)), sqrt(2)),
         (((1,1), (2,2)), sqrt(2)),
         ...
         ]
     '''
-
+    edge_list = edge_list[:]    # or mst() while clear argement edge_list after calculate.
     edge_list.sort(key=lambda (e, el): el)
+    edge_list.reverse()
    
-    p_count = len(set(
-        p
-        for e, el in edge_list
-        for p in e
-        ))
-
     mst = []
+    
     p_in_mst = set()
 
     while edge_list:
-        #((x1, y1), (x2, y2) = edge_list.pop()
-        pass
+        (e, el) = edge_list.pop()
+        (p1, p2) = e
+        if all((
+            p1 in p_in_mst,
+            p2 in p_in_mst,
+        )):
+            continue
+        else:
+            mst.append(e)
+            p_in_mst.update(e)
 
     return mst
